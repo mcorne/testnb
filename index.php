9a8384ad10d82dcec6821736b489e30bec1b2d78
Michel Corne <mcorne@yahoo.com>
2018-01-06 20:00:56 +0100

    test

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-PHP 7.0.3 (cli) (built: Feb  2 2016 14:41:12) ( ZTS )
-Copyright (c) 1997-2016 The PHP Group
-Zend Engine v3.0.0, Copyright (c) 1998-2016 Zend Technologies
-    with Xdebug v2.4.0RC4, Copyright (c) 2002-2016, by Derick Rethans
+123
